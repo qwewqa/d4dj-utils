9de479371ecd0deb55e6acd33277b5734883f804
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-11-20 15:24:39 -0800

    Adjust stamp restrictions

--- d4dj_utils/master/stamp_master.py
+++ d4dj_utils/master/stamp_master.py
@@ -19,8 +19,8 @@ class StampMaster(MasterAsset):
 
     @property
     def is_released(self):
-        return (self.id < 10000 or
-                (10000 < self.id < 20000 and
+        return (self.id <= 10036 or
+                (10036 < self.id < 20000 and
                  (self.id - 10000) in self.assets.event_master and
                  self.assets.event_master[self.id - 10000].is_released) or
                 (20000 < self.id < 30000 and self.id <= 20026) or  # Hardcoding these two until a better way is found
