239a5a9a2982d48432c3f51e51070d8c277814fd
Mirby <mirbyc123@gmail.com>
2024-11-11 11:48:44 -0500

    update stamps to include all

--- d4dj_utils/master/stamp_master.py
+++ d4dj_utils/master/stamp_master.py
@@ -1,7 +1,7 @@
 import re
 from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
 from d4dj_utils.master.master_asset import MasterAsset
 
@@ -13,24 +13,27 @@ class StampMaster(MasterAsset):
     name: str
     description: str
     has_voice: bool
+    duplicate_id: int
+    character_ids: Tuple[int, ...]
 
     def __hash__(self):
         return self.id.__hash__()
 
     @property
     def is_released(self):
-        return (
-            self.id <= 10036
-            or (
-                10036 < self.id < 20000
-                and (self.id - 10000) in self.assets.event_master
-                and self.assets.event_master[self.id - 10000].is_released
-            )
-            or (20000 < self.id < 30000 and self.id <= 20026)
-            or (  # Hardcoding these two until a better way is found
-                30000 < self.id < 40000 and self.id <= 30001
-            )
-        )
+        return True
+        # return (
+        #     self.id <= 10036
+        #     or (
+        #         10036 < self.id < 20000
+        #         and (self.id - 10000) in self.assets.event_master
+        #         and self.assets.event_master[self.id - 10000].is_released
+        #     )
+        #     or (20000 < self.id < 30000 and self.id <= 20026)
+        #     or (  # Hardcoding these two until a better way is found
+        #         30000 < self.id < 40000 and self.id <= 30001
+        #     )
+        # )
 
     @property
     def quote(self):
